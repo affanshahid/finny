a29e395ac4957e5b35f55bfc3fdfb5770d5e56af
Affan Shahid <affan.shahid.94@gmail.com>
2022-09-15 03:01:15 +0500

    refact: use iterator to return messages

--- src/message.rs
+++ src/message.rs
@@ -75,7 +75,7 @@ impl TextMessage {
         let conn = Connection::open(home)?;
         let mut stmt = conn.prepare(&QUERY.replace("{IDS}", &in_param))?;
 
-        let msgs = stmt.query_map(
+        let mut msgs = stmt.query_map(
             params![
                 start.timestamp_nanos() - NSECS_SINCE_2001,
                 (end.timestamp_nanos() - NSECS_SINCE_2001),
@@ -89,15 +89,11 @@ impl TextMessage {
             },
         )?;
 
-        let mut result = Vec::new();
-        for msg in msgs {
-            match msg {
-                Ok(msg) => result.push(msg),
-                Err(err) => return Err(Error::SqliteError(err)),
-            }
+        if let Some(err) = msgs.find(|m| m.is_err()) {
+            err?;
         }
 
-        Ok(result)
+        Ok(msgs.map(|m| m.unwrap()).collect())
     }
 
     fn parse_time_from_century_epoch(century_epoch: i64) -> DateTime<Utc> {
