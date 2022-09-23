a629f863c58757c8c1fb7233dcf5090ea3d25bc1
Affan Shahid <affan.shahid.94@gmail.com>
2022-09-23 19:03:29 +0500

    refact: move calculations to process

--- src/process.rs
+++ src/process.rs
@@ -1,3 +1,13 @@
+use lazy_static::lazy_static;
+use rust_decimal::Decimal;
+use rust_decimal_macros::dec;
+use rusty_money::iso;
+use rusty_money::iso::Currency;
+use rusty_money::Exchange;
+use rusty_money::ExchangeRate;
+use rusty_money::Money;
+
+use crate::record::Nature;
 use crate::record::Record;
 
 pub fn filter_out_sources<'a>(records: &Vec<Record<'a>>, sources: &Vec<String>) -> Vec<Record<'a>> {
@@ -29,4 +39,54 @@ pub fn fuzzy_filter_in_sources<'a>(
                 .any(|s| r.source.to_lowercase().contains(&s.to_lowercase()))
         })
         .collect()
+}
+
+pub const NORMALIZED_CURRENCY: &Currency = iso::PKR;
+
+lazy_static! {
+    //FIXME: remove when https://github.com/varunsrin/rusty_money/pull/75 is merged
+    static ref RATES: Vec<ExchangeRate<'static, Currency>> =
+        vec![
+            ExchangeRate::new(iso::USD, iso::PKR, dec!(237)).unwrap(),
+            ExchangeRate::new(iso::SGD, iso::PKR, dec!(158)).unwrap()
+        ];
+    static ref EXCHANGE: Exchange<'static, Currency> = {
+        let mut exchange: Exchange<'static, Currency> = Exchange::new();
+        for rate in RATES.iter() {
+            exchange.set_rate(&rate);
+        }
+        exchange
+    };
+}
+
+pub fn calculate_total(records: &Vec<Record>) -> Money<'static, Currency> {
+    records
+        .iter()
+        .map(|r| normalize_amount(r).amount().clone())
+        .reduce(|accum, current| accum + current)
+        .map(|total| Money::from_decimal(total, NORMALIZED_CURRENCY))
+        .unwrap()
+}
+
+pub fn normalize_amount(r: &Record) -> Money<'static, Currency> {
+    //FIXME: remove clone when https://github.com/varunsrin/rusty_money/pull/76 is merged
+    let mut result = r.amount.clone();
+
+    if r.amount.currency() != NORMALIZED_CURRENCY {
+        let rate = EXCHANGE
+            .get_rate(r.amount.currency(), NORMALIZED_CURRENCY)
+            .expect(&format!(
+                "currency rate not configured: {}",
+                r.amount.currency().iso_alpha_code
+            ));
+
+        result = rate.convert(result).unwrap();
+        let _f = 1;
+    }
+
+    if let Nature::Debit = r.nature {
+        result = Money::from_decimal(result.amount() * Decimal::NEGATIVE_ONE, result.currency());
+    }
+
+    result
 }
--- src/viewer.rs
+++ src/viewer.rs
@@ -11,35 +11,10 @@ use comfy_table::Color;
 use comfy_table::ContentArrangement;
 use comfy_table::Row;
 use comfy_table::Table;
-use lazy_static::lazy_static;
-use rust_decimal::Decimal;
-use rust_decimal_macros::dec;
-use rusty_money::iso;
-use rusty_money::iso::Currency;
-use rusty_money::Exchange;
-use rusty_money::ExchangeRate;
-use rusty_money::Money;
 
+use crate::process;
 use crate::record::Nature;
 use crate::record::Record;
-
-pub const NORMALIZED_CURRENCY: &Currency = iso::PKR;
-
-lazy_static! {
-    //FIXME: remove when https://github.com/varunsrin/rusty_money/pull/75 is merged
-    static ref RATES: Vec<ExchangeRate<'static, Currency>> =
-        vec![
-            ExchangeRate::new(iso::USD, iso::PKR, dec!(237)).unwrap(),
-            ExchangeRate::new(iso::SGD, iso::PKR, dec!(158)).unwrap()
-        ];
-    static ref EXCHANGE: Exchange<'static, Currency> = {
-        let mut exchange: Exchange<'static, Currency> = Exchange::new();
-        for rate in RATES.iter() {
-            exchange.set_rate(&rate);
-        }
-        exchange
-    };
-}
 
 pub struct Viewer<'a> {
     show_matchers: bool,
@@ -62,7 +37,7 @@ impl Viewer<'_> {
             row.add_cell(Cell::new(&r.matcher.id));
         }
         row.add_cell(Cell::new(&r.source));
-        row.add_cell(Cell::new(Self::normalized_amount(&r)).fg(match r.nature {
+        row.add_cell(Cell::new(process::normalize_amount(&r)).fg(match r.nature {
             Nature::Credit => Color::Green,
             Nature::Debit => Color::Red,
         }));
@@ -70,14 +45,8 @@ impl Viewer<'_> {
         row
     }
 
-    fn compute_total_row(&self) -> Row {
-        let total = self
-            .records
-            .iter()
-            .map(|r| Self::normalized_amount(r).amount().clone())
-            .reduce(|accum, current| accum + current)
-            .map(|total| Money::from_decimal(total, NORMALIZED_CURRENCY))
-            .unwrap();
+    fn create_total_row(&self) -> Row {
+        let total = process::calculate_total(&self.records);
 
         let mut row = vec![
             Cell::new(""),
@@ -96,30 +65,6 @@ impl Viewer<'_> {
             row.insert(0, Cell::new(""));
         }
         row.into()
-    }
-
-    fn normalized_amount(r: &Record) -> Money<'static, Currency> {
-        //FIXME: remove clone when https://github.com/varunsrin/rusty_money/pull/76 is merged
-        let mut result = r.amount.clone();
-
-        if r.amount.currency() != NORMALIZED_CURRENCY {
-            let rate = EXCHANGE
-                .get_rate(r.amount.currency(), NORMALIZED_CURRENCY)
-                .expect(&format!(
-                    "currency rate not configured: {}",
-                    r.amount.currency().iso_alpha_code
-                ));
-
-            result = rate.convert(result).unwrap();
-            let _f = 1;
-        }
-
-        if let Nature::Debit = r.nature {
-            result =
-                Money::from_decimal(result.amount() * Decimal::NEGATIVE_ONE, result.currency());
-        }
-
-        result
     }
 }
 
@@ -144,7 +89,7 @@ impl Display for Viewer<'_> {
                     .map(|r| self.record_to_row(r))
                     .collect::<Vec<_>>(),
             )
-            .add_row(self.compute_total_row());
+            .add_row(self.create_total_row());
 
         table.fmt(f)
     }
